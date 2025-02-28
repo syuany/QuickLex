b48877a489cfb646ee62d9d67160c8eecc972fb6
syuany <sean7790@qq.com>
2025-02-28 17:00:26 +0800

    add note

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ from core.hotkey import HotkeyManager
 
 
 def main():
+    # 创建QApplication实例并设置退出行为
     app = QApplication(sys.argv)
     app.setQuitOnLastWindowClosed(False)
 
@@ -14,19 +15,24 @@ def main():
     #     import ctypes
     #     ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID("dictapp")
 
+    # 创建主窗口实例
     window = MainWindow()
 
+    # 设置托盘图标路径并创建托盘图标实例
     icon_path = r"E:\workspace\class_101\QuickLex\resources\icon.png"
     tray = TrayIcon(window, icon_path)
 
+    # 创建热键管理器实例并注册热键
     hotkey = HotkeyManager(window)
     app.installNativeEventFilter(hotkey)
     hotkey.register_hotkey()
 
+    # 显示托盘图标和主窗口
     tray.show()
     window.show()
     # window.hide()
 
+    # 进入应用程序主循环
     sys.exit(app.exec_())
 
 
