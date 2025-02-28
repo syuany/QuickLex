17ce8d9e595b20b246a24fa3af18cb3a1a766ea8
syuany <sean7790@qq.com>
2025-02-28 17:00:48 +0800

    add note

--- src/core/hotkey.py
+++ src/core/hotkey.py
@@ -6,32 +6,32 @@ import ctypes
 
 class HotkeyManager(QAbstractNativeEventFilter):
     def __init__(self, window):
+        # 初始化HotkeyManager，设置窗口和热键ID
         super().__init__()
         self.window = window
         self.hotkey_id = 1
         self.registered = False
 
     def register_hotkey(
-        # self, modifiers=win32con.MOD_ALT | win32con.MOD_CONTROL, key=ord("D")
         self,
         modifiers=win32con.MOD_CONTROL,
         key=ord("1"),
     ):
+        # 注册热键，设置修饰键和键值
         if not self.registered:
             try:
                 win32gui.RegisterHotKey(None, self.hotkey_id, modifiers, key)
                 self.registered = True
-                # print(f"Hotkey registered successfully: {modifiers}, {key}")
             except Exception as e:
                 print(f"Failed: register hotkey:{str(e)}")
                 self.registered = False
 
     def nativeEventFilter(self, event_type, message):
+        # 处理原生事件，检测热键触发
         if event_type == "windows_generic_MSG":
             msg = ctypes.wintypes.MSG.from_address(message.__int__())
             if msg.message == win32con.WM_HOTKEY:
                 if msg.wParam == self.hotkey_id:
-                    # print("Hotkey triggered!")  # 调试信息
                     QTimer.singleShot(0, self.window.toggle_visibility)
                     return True, 0
         return False, 0
